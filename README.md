10938347b0d1f67cf24f9166faa871402b10edad
root <vneelam609@gmail.com>
2018-11-08 11:36:34 +0000

    Latest commit with changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # programs
+
+<h2>Wordpress installation for Debian based systems</h2>
+<p>LEMP Stack<p>
+<li>Linux</li>
+<li>Nginx</li>
+<li>Mysql</li>
+<li>PHP</li>
+
+This script automates the task required to setup LEMP stack and get your site running
+
+<h3>Tested on <em>Ubuntu 18.04</em></h3>
+
--- wp.sh
+++ wp.sh
@@ -6,7 +6,8 @@
 
 dearchive() {
 	echo "${green} [*]De-archiving wordpress..${reset}"
-	unzip -j /var/www/$domain/wordpress.zip -d /var/www/$domain/
+	unzip /var/www/$domain/wordpress.zip 
+	rsync -avP ~/wordpress/ /var/www/$domain
 	echo "${green} [*]Wordpress extracted successfully ${reset}"
 	echo "${logdate} Extraction completed succesffully." >> $log
 }
@@ -33,32 +34,47 @@ logdate=`date`
 #Check for installed applications
 echo "Querying list of programs required for this operation"
 
-if [ "$(dpkg-query -l|grep -i -c "ngnix")" -ge 0 ]; then
+if [ "$(dpkg-query -l|grep -i -c "ngnix")" -gt 0 ]; then
 	echo "${green} [*] Nginx found.${reset}"
+	sudo service nginx start
+	echo "${logdate} Nginx service started succesfully" >> $log
 	echo "${logdate} Ngnix found" >> $log
 else
 	echo "${green}*Installing nginx${reset}"
 	sudo apt-get install -y nginx
-	echo "${logdate} Nginx installed" >> $log
+	sudo service nginx start
+	echo "${logdate} Nginx service started successfully" >> $log
+ 	echo "${logdate} Nginx installed" >> $log
 fi
 
-if [ "$(dpkg-query -l|grep -i -c "mysql")" -ge 0 ]; then
+if [ "$(dpkg-query -l|grep -i -c "mysql")" -gt 0 ]; then
 	echo "${green} [*] Mysql found.${reset}"
+	sudo service mysql start
+	echo "${logdate} Mysql service starteed succesfully" >> $log
 	echo "${logdate} Mysql found" >> $log
 else
 	echo "${green}*Installing mysql${reset}"
-	sudo apt-get install -y mysql
+	#export DEBIAN_FRONTEND=noninteractive
+	#sudo -E apt-get -q -y install mysql-server
+	echo "mysql-server mysql-server/root_password password temp" | sudo debconf-set-selections
+	echo "mysql-server mysql-server/root_password_again password temp" | sudo debconf-set-selections
+	#sudo apt-get -y install mysql-server mysql-client
+	sudo service mysql start
+	echo "${logdate} Mysql service started successfully" >> $log
 	echo "${logdate} Mysql installed" >> $log
 fi
 
-if [ "$(dpkg-query -l|grep -i -c "php")" -ge 0 ]; then
+if [ "$(dpkg-query -l|grep -i -c "php")" -gt 0 ]; then
 	echo "${green} [*] PHP found.${reset}"
 	echo "${logdate} PHP found" >> $log
 else
 	echo "${green}*Installing PHP${reset}"
-	sudo apt-get install php
+	sudo apt-get install -y php php-mysql php-fpm
+	sudo systecmtl start php7.2-fpm
 	echo "${logdate} PHP installed." >> $log
 fi
+
+
 
 #Read domain name from the user
 
@@ -97,15 +113,19 @@ sudo cp $nginx_default /tmp/default
 sudo cp $nginx_default $nginx_enabled
 #Define necessary changes
 #Add index.php for php support
+#sed -i 's/root\s\/var\/www\/html\/;/root\s/\/var\/www\/$domain\;/g' $nginx_enabled
+sed -i "s/root\s\/var\/www\/html\;/root \/var\/www\/$domain\;/g" $nginx_enabled
+sed -i 's/fastcgi_pass unix\:\/var\/run\/php\/php7.0-fpm.sock\;/fastcgi_pass unix\:\/var\/run\/php\/php7.2-fpm.sock\;/g' $nginx_enabled 
+#sed -i 's/fastcgi_pass\sunix:\/var\/run\/php7.0-fpm.sock\;/fastcgi_pass\sunix:\/var\/run\/php7.2-fpm.sock\;/g' $nginx_enabled
 sed -i 's/index index.html index.htm index.nginx-debian.html\;/index index.html index.php index.htm index.nginx-debian.html\;/g' $nginx_enabled
 sed -i "s/server_name example.com/server_name $domain/g" $nginx_enabled
 sed -i "85{s/root/#/}" $nginx_enabled
-sed -i "86iroot /var/www/$domain\;" $nginx_enabled
+#sed -i "86iroot /var/www/$domain\;" $nginx_enabled
 #Uncomment necessary lines
 sed -i "56,61{s/#//}" $nginx_enabled 
 sed -i "63{s/#//}" $nginx_enabled
 sed -i "68,70{s/#//}" $nginx_enabled 
-sed -i "79,92{s/#//}" $nginx_enabled 
+#sed -i "79,92{s/#//}" $nginx_enabled 
 
 #Hide the default config which is running at port 80 and enable our config
 
@@ -128,30 +148,36 @@ fi
 echo "${green} [*]Preparing to download the latest version of wordpress ${reset}"
 echo "${logdate} Downloading latest version of Wordpress" >> $log
 
+
+sudo apt-get install -y curl
+	
+
 curl -L http://wordpress.org/latest.zip -o wordpress.zip 
 
 sudo mkdir /var/www/$domain
 echo "${logdate} Moving wordpress to /var/www/$domain" >> $log
 sudo mv wordpress.zip /var/www/$domain/ 
 echo "${logdate} File moved successfully to /var/www/$domain" >> $log
-if [ "$(dpkg-query -l|grep "unzip")" -ge 1 ]; then 
+if [ "$(dpkg-query -l|grep "unzip")" -gt 1 ];  then 
 	dearchive	
 else
 	echo "${red} Unzip not installed ${reset}";
 	echo "${green} Installing unzip ${reset}";
-	sudo apt-get install unzip
+	sudo apt-get install -y unzip
 	dearchive
 fi
+
 
 #Create database
 postfix="_db"
 dbname=$domain$postfix
 echo $dbname
-echo "${green} [-p] Enter root password ${reset}"
-stty -echo
-read  dbpass
-stty echo
-mysql -u root -p$dbpass <<EOF
+#echo "${green} [-p] Enter root password ${reset}"
+#stty -echo
+#read  dbpass
+#stty echo
+dbpass=temp
+mysql -u root -p$dbpass  <<EOF
 CREATE DATABASE IF NOT EXISTS \`$dbname\` CHARACTER SET utf8 COLLATE utf8_general_ci
 EOF
 if [ $? -ne 0 ]; then
@@ -177,6 +203,14 @@ then
 	sed -i "s/database_name_here/$dbname/g" $config_path/wp-config.php
 	sed -i "s/username_here/root/g" $config_path/wp-config.php
 	sed -i "s/password_here/$dbpass/g" $config_path/wp-config.php
+	
+	#define keys for wp-config
+	salts_keys=$(curl https://api.wordpress.org/secret-key/1.1/salt) 
+	salts_keys=$(echo $salts_keys | sed -e 's/\([[\/.*]\|\]\)/\\&/g')
+	
+	sed -i "/_KEY/d" /var/www/$domain/wp-config.php >> $log
+	sed -i "/_SALT/d" /var/www/$domain/wp-config.php >> $log
+	sed -i "/define('DB_COLLATE', '');/a$salts_keys" /var/www/$domain/wp-config.php
 	echo "${green} [*] Config generated successfully. ${reset}"
 	echo "${logdate} wp-config generated succesfully. ${reset}" >> $log
 else
@@ -185,6 +219,8 @@ else
 fi
 
 #Fix permissions
+chown -R :www-data /var/www/$domain/
+chmod 755 /var/www/$domain/
 sudo chmod 644 /var/www/$domain/wp-config.php
 #remove zip file
 #sudo rm -r /var/www/$domain/wordpress.zip
